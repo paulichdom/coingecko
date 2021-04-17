595709337ddb73a0d9cde0c110076a0cf29bb3d6
Domagoj <paulic.domagoj@gmail.com>
2021-04-17 12:18:29 +0200

    C13, add Style-CoinListItem

--- src/components/CoinListItem.tsx
+++ src/components/CoinListItem.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Table, List, Image, Icon } from 'semantic-ui-react';
+import { Table, List, Image, Icon, Container } from 'semantic-ui-react';
 
 import ICoinListItem from '../types/ICoinListItem';
 
@@ -18,6 +18,12 @@ export default function CoinListItem(props: Props): ReactElement {
     currency: props.currency,
   });
 
+  const formatCurrencyDigits = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: props.currency,
+    minimumFractionDigits: 0,
+  });
+
   const onGoToDetail = () => {
     history.push(`/coins/${coin.id}`);
   };
@@ -31,36 +37,71 @@ export default function CoinListItem(props: Props): ReactElement {
             <Image avatar src={coin.image} />
             <List.Content>
               <List.Header>
-                {coin.name}{' '}
-                <small>
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </small>
+                {coin.name} <span id="grey"> {coin.symbol.toUpperCase()}</span>
               </List.Header>
             </List.Content>
           </List.Item>
         </List>
       </Table.Cell>
-      <Table.Cell>{formatCurrency.format(coin.current_price)}</Table.Cell>
-      <Table.Cell>
-        {coin.price_change_percentage_1h_in_currency
-          ? Math.abs(+coin.price_change_percentage_1h_in_currency.toFixed(1))
-          : 'n/a'}{' '}
-        %
+      <Table.Cell className="bold">
+        {formatCurrency.format(coin.current_price)}
       </Table.Cell>
       <Table.Cell>
-        {coin.price_change_percentage_24h_in_currency
-          ? Math.abs(+coin.price_change_percentage_24h_in_currency.toFixed(1))
-          : 'n/a'}{' '}
-        %
+        <div
+          id={coin.price_change_percentage_1h_in_currency > 0 ? 'green' : 'red'}
+          className="bold"
+        >
+          {coin.price_change_percentage_1h_in_currency > 0 ? (
+            <Icon name="caret up" />
+          ) : (
+            <Icon name="caret down" />
+          )}
+          {coin.price_change_percentage_1h_in_currency
+            ? Math.abs(+coin.price_change_percentage_1h_in_currency.toFixed(1))
+            : 'n/a'}{' '}
+          %
+        </div>
       </Table.Cell>
       <Table.Cell>
-        {coin.price_change_percentage_7d_in_currency
-          ? Math.abs(+coin.price_change_percentage_7d_in_currency.toFixed(1))
-          : 'n/a'}{' '}
-        %
+        <div
+          id={
+            coin.price_change_percentage_24h_in_currency > 0 ? 'green' : 'red'
+          }
+          className="bold"
+        >
+          {coin.price_change_percentage_24h_in_currency > 0 ? (
+            <Icon name="caret up" />
+          ) : (
+            <Icon name="caret down" />
+          )}
+          {coin.price_change_percentage_24h_in_currency
+            ? Math.abs(+coin.price_change_percentage_24h_in_currency.toFixed(1))
+            : 'n/a'}{' '}
+          %
+        </div>
       </Table.Cell>
-      <Table.Cell>{formatCurrency.format(coin.total_volume)}</Table.Cell>
-      <Table.Cell>{formatCurrency.format(coin.market_cap)}</Table.Cell>
+      <Table.Cell>
+        <div
+          id={coin.price_change_percentage_7d_in_currency > 0 ? 'green' : 'red'}
+          className="bold"
+        >
+          {coin.price_change_percentage_7d_in_currency > 0 ? (
+            <Icon name="caret up" />
+          ) : (
+            <Icon name="caret down" />
+          )}
+          {coin.price_change_percentage_7d_in_currency
+            ? Math.abs(+coin.price_change_percentage_7d_in_currency.toFixed(1))
+            : 'n/a'}{' '}
+          %
+        </div>
+      </Table.Cell>
+      <Table.Cell className="bold">
+        {formatCurrencyDigits.format(coin.total_volume)}
+      </Table.Cell>
+      <Table.Cell className="bold">
+        {formatCurrencyDigits.format(coin.market_cap)}
+      </Table.Cell>
     </Table.Row>
   );
 }
